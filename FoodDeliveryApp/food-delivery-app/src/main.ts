2e42ad9de5258ded048fdfc4a2abf1bb3bcce4d2
Riwaj Rai <riwajrai83@gmail.com>
2024-12-18 18:38:24 -0500

    Remove App-Delivery-App and to recreate it

--- FoodDeliveryApp/food-delivery-app/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(withFetch()), // Enables Fetch API
-  ],
-});
