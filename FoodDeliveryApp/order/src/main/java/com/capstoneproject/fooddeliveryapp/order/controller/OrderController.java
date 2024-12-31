4facdcad26a01dc2fde1187b234aceed9852760f
Riwaj Rai <riwajrai83@gmail.com>
2024-12-30 23:06:49 -0500

    Added the final Order MS with the functionality of adding the order in the cart

--- FoodDeliveryApp/order/src/main/java/com/capstoneproject/fooddeliveryapp/order/controller/OrderController.java
+++ FoodDeliveryApp/order/src/main/java/com/capstoneproject/fooddeliveryapp/order/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.capstoneproject.fooddeliveryapp.order.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.capstoneproject.fooddeliveryapp.order.service.OrderService;
 
 @RestController
 @RequestMapping("/order")
+@CrossOrigin
 public class OrderController {
 	
 	@Autowired
